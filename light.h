71e8afb268b72b8dbfa884861b99cf0c331a426f
Matt MacEwan(Desk) <mrm4677@rit.edu>
2012-05-20 01:30:29 -0400

    Flailing about with Light class
    Engineering and class design is for lameos.

---
+++ light.h
@@ -0,0 +1,26 @@
+// File: light.h
+// Author: Matthew MacEwan
+// Classes and global values supporting various lighting scenarios
+// ///////////////////////////////////////////////////////////////////// 
+
+#ifndef LIGHT_H_
+#define LIGHT_H_
+
+#include "scene.h"
+
+/// Class for positional & directional lights
+/// but not ambient light.
+class Light : public Geometry {
+private:
+	/// OpenGL light constant assigned to this light
+	const int lightHandle;
+
+public:
+	/// RGBA light color
+	float color[4];
+
+protected:
+	virtual void doRender();
+}
+
+#endif
